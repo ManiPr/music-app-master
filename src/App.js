4749fc1e0095629698231fa027cf8e98981c87fd
ManiGeek <maniweeb@gmail.com>
2023-06-14 12:37:15 +0330

    feat:routing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Nav from './components/nav/Nav'
 import routes from './routes'
 import { useRoutes } from 'react-router-dom'
 import Music from './components/music/Music'
+import BrowseMenu from './components/browse-menu/BrowseMenu'
 import PlayMusic from './components/music-player/MusicPlayer'
-import GenresMoods from './pages/browser/genres-moods/GenresMoods'
 export default function App() {
   let router=useRoutes(routes)
   return (
     <>
-      <Nav></Nav>
-      <main className='main'>
+    <Nav></Nav>
+    <main className='main'>
         <div className='container'>
           {router}
         </div>
         </main>
         <PlayMusic></PlayMusic>
-        <GenresMoods></GenresMoods>
     </>
   );
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,8 +2,13 @@ import React from 'react'
 import user from './../../assets/images/SVG File/Profle 30.svg'
 import dash from './../../assets/images/SVG File/Path 690.svg'
 import logo from './../../assets/images/SVG File/Full Logo.svg'
+import {Link} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
+
 import './Nav.css'
 export default function Nav() {
+  let location=useLocation()
+
   return (
     <nav className='nav'>
         <div className='container'>
@@ -11,12 +16,19 @@ export default function Nav() {
               <div className='nav__left center'>
                 <img className='nav__logo' src={logo}/>
                 <ul className='nav__menus center'>
-                  <li className='nav__menu'>Browse</li>
-                  <li className='nav__menu'>Library</li>
-                  <li className='nav__menu'>Home</li>
+                  <Link className='nav__menu' to='/browse'>Browse</Link>
+                  <Link className='nav__menu'>Library</Link>
+                  <Link className='nav__menu'>Home</Link>
                 </ul>
               </div>
-                <div className='nav__right center'>
+              {location.pathname==='/browse'&&
+               <div className='browse-menus center'>
+               <Link className='browse-menu' to='/browse'>Genres & Moods</Link>
+               <Link className='browse-menu' to='/NewReleases'>New Releases</Link>
+               <Link className='browse-menu' to='/Podcasts'>Podcasts</Link>
+             </div>
+             }
+             <div className='nav__right center'>
                 <input type='text' className='nav__serch' placeholder='Search'/>
                 <div className='nav__personal center'>
                   <img src={user}></img>
